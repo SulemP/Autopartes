37d346b24641881942ec357d13b048ec0ea12d8d
SulemP <sulee.ths@gmail.com>
2022-05-28 17:14:38 +0200

    Version 2.0

---
+++ vista/modulos/footer.html.php
@@ -0,0 +1,8 @@
+<div class="container-fluid" id="footer">
+    <div class="container">
+        <p class="small copy__p">
+          &copy; 2017 todos los derechos reservados por <a class="bp" href="#">Blackparadox</a>
+        </p>
+    </div>
+    <div class="footer__final"></div> 
+</div>
