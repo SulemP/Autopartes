1fbd9b6d4887d56cb5723e9b668bc594d4629406
SulemP <sulee.ths@gmail.com>
2022-05-28 17:11:11 +0200

    Se agrega el modulo de adicionales

---
+++ vista/modulos/adicionales.html.php
@@ -0,0 +1,10 @@
+<div class="container-fluid" id="adicional">
+    <img class="movimiento" src="vista/asset/images/movimiento.gif" alt="">
+    <div class="row">
+        <div class="col-5 informacion">
+            <h4 class="adicional__h">Te ayudamos en lo que necesites</h4>
+            <p class="adicional__p">Mantener tu auto impecable es la mejor decisión.</p>
+            <p class="adicional__info">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+        </div>
+    </div>
+</div>
